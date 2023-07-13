d62ea6f96d9f63f9c49eca8f0c41df5b5d6417ba
quanhspdz <pc.quanhdz@gmail.com>
2023-07-13 16:38:58 +0700

    add new feature: text to speech

--- app/src/main/java/com/example/textrecognition/TextResultActivity.kt
+++ app/src/main/java/com/example/textrecognition/TextResultActivity.kt
@@ -2,20 +2,43 @@ package com.example.textrecognition
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
 import com.example.textrecognition.databinding.ActivityTextRecognitionBinding
 import com.example.textrecognition.databinding.ActivityTextResultBinding
+import java.util.Locale
 
 class TextResultActivity : AppCompatActivity() {
-    lateinit var binding: ActivityTextResultBinding
+    private lateinit var binding: ActivityTextResultBinding
+    private var textResult: String? = null
+    private lateinit var textToSpeech: TextToSpeech
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityTextResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val textResult = intent.getStringExtra("result")
+        textToSpeech = TextToSpeech(this) { status ->
+            if (status != TextToSpeech.ERROR) {
+                // Set the language for text-to-speech
+                textToSpeech.language = Locale.US
+            }
+        }
+
+        textResult = intent.getStringExtra("result")
 
         if (textResult != null)
             binding.textResult.text = textResult
+
+        binding.buttonSpeaker.setOnClickListener {
+            if (textResult != null) {
+                textToSpeech.speak(textResult, TextToSpeech.QUEUE_FLUSH, null, null)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        // Shutdown the Text-to-Speech engine
+        textToSpeech.shutdown()
     }
 }
